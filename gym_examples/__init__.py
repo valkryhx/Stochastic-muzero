7c5dc58c3bb763447130421ac96a21f2914deb00
valkryhx <283306449@qq.com>
2024-02-27 18:18:18 +0800

    Update __init__.py

--- gym_examples/__init__.py
+++ gym_examples/__init__.py
@@ -2,7 +2,7 @@ from gym_examples.envs.grid_world import GridWorldEnv
 from gymnasium.envs.registration import register
 
 register(
-     id="MyGridWorld-v0",
+     id="GridWorld-v0",
      entry_point="gym_examples:GridWorldEnv",
      max_episode_steps=300,
 )
